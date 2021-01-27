3b4c5b61ec04ae6c936c6a4731702c337eec6d3f
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-26 20:53:26 -0500

    Training

--- app/App.js
+++ app/App.js
@@ -1,18 +1,18 @@
 "use strict";
-import Header from "./components/Header.js";
+import Top from "./components/Top.js";
 import Title from "./components/Title.js";
 import Nav from "./components/Nav.js";
 import Main from "./components/Main.js";
-import Footer from "./components/Footer.js";
+import Bottom from "./components/Bottom.js";
 import Router from "./components/Router.js";
 // call start components and router
 export function App() {
   const $body = document.querySelector("body");
   $body.innerHTML = null;
-  $body.appendChild(Header());
+  $body.appendChild(Top());
   $body.appendChild(Title());
   $body.appendChild(Nav());
   $body.appendChild(Main());
-  $body.appendChild(Footer());
+  $body.appendChild(Bottom());
   Router();
 }
