77c453f98d3e5ed7c3ef62a83c28174763d7045f
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-03-10 17:40:40 -0500

    Training

--- app/components/Bottom.js
+++ app/components/Bottom.js
@@ -1,17 +1,21 @@
-"use strict";
+'use strict';
 // set up the Bottom component
 export default function Bottom() {
-  const $bottom = document.createElement("div");
-  $bottom.classList.add("dev");
+  const $bottom = document.createElement('div');
+  $bottom.classList.add('dev');
   $bottom.innerHTML = `
-<div class="bottom">
-  © Built by
-  <a
-    href="https://www.javierandresgp.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    >Javier Andrés GP</a
-  >
+<div class="dev">
+  <div class="bottom">
+    <a
+      href="https://www.javierandresgp.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <span>© Built by</span>
+      <img src="./app/assets/avatar.png" alt="Javier Andrés GP" />Javier Andrés
+      GP</a
+    >
+  </div>
 </div>
   `;
   return $bottom;
--- app/components/Top.js
+++ app/components/Top.js
@@ -1,17 +1,21 @@
-"use strict";
+'use strict';
 // set up the Top component
 export default function Top() {
-  const $top = document.createElement("div");
-  $top.classList.add("dev");
+  const $top = document.createElement('div');
+  $top.classList.add('dev');
   $top.innerHTML = `
-<div class="top">
-  © Built by
-  <a
-    href="https://www.javierandresgp.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    >Javier Andrés GP</a
-  >
+<div class="dev">
+  <div class="top">
+    <a
+      href="https://www.javierandresgp.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <span>© Built by</span>
+      <img src="./app/assets/avatar.png" alt="Javier Andrés GP" />Javier Andrés
+      GP</a
+    >
+  </div>
 </div>
   `;
   return $top;
